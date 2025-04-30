17c653ea48b848fde1409a0d5881b270341d1a5b
nasa7792 <nasa7792@colorado.edu>
2025-04-30 01:41:09 -0600

    update ui

--- react-front-end-code/pi-net-device/src/Sensor.jsx
+++ react-front-end-code/pi-net-device/src/Sensor.jsx
@@ -18,7 +18,7 @@ function Sensor() {
 
   useEffect(() => {
     const interval = setInterval(() => {
-      fetch("http://10.0.0.103:8080/")
+      fetch("http://172.20.10.3:8080/")
         .then((response) => response.text())
         .then((text) => setSensorData(text))
         .catch(() => setSensorData("Error"));
@@ -36,42 +36,63 @@ function Sensor() {
         overflow: "hidden",
         width: "100vw",
         height: "100vh",
+        backgroundColor: "#87CEEB", // fallback background
       }}
     >
-<div
-  style={{
-    backgroundImage: 'url("https://www.clker.com/cliparts/K/p/0/3/I/e/purple-car-hi.png")',
-    backgroundRepeat: "no-repeat",
-    backgroundSize: "100px auto", // resize image
-    backgroundPosition: "center", // or 'center center' etc.
-    width: "200%",
-    height: "100%",
-    animation: `scroll ${scrollSpeed} linear infinite`,
-   animationPlayState: scrollSpeed === "20s" ? "paused" : "running",
-  }}
-/>
+      {/*create an effect of background scroll*/}
+      <div
+        style={{
+          backgroundImage:
+            'url("https://static.vecteezy.com/system/resources/thumbnails/004/867/154/small_2x/an-empty-straight-road-near-the-river-forest-on-the-horizon-cartoon-style-illustration-vector.jpg")',
+          backgroundRepeat: "repeat-x",
+          backgroundSize: "auto 100%", // keep original width, fit height
+          backgroundPosition: "bottom", // align to bottom if needed
+          width: "200%",
+          height: "100%",
+          animation: `scroll ${scrollSpeed} linear infinite`,
+          animationPlayState: scrollSpeed === "20s" ? "paused" : "running",
+        }}
+      />
 
+      {/* car image remains static */}
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/Red_Car_Closed_Window_Cartoon_Vector.svg/2560px-Red_Car_Closed_Window_Cartoon_Vector.svg.png"
+        alt="Car"
+        style={{
+          position: "absolute",
+          bottom: "10%",
+          left: "50%",
+          transform: "translateX(-50%)",
+          width: "200px", // adjust size as needed
+          height: "auto",
+          zIndex: 2,
+        }}
+      />
+
+      {/* display proximity data */}
       <div
         style={{
           position: "absolute",
           top: 20,
           left: 20,
-          color: "black",
+          color: "white",
           fontSize: "2rem",
           fontWeight: "bold",
           textShadow: "0 0 10px black",
+          zIndex: 3,
         }}
       >
         Proximity: {sensorData}
       </div>
 
+      {/* Keyframes */}
       <style>
         {`
-          @keyframes scroll {
-  0% { transform: translateX(-50%); }
-  100% { transform: translateX(0); }
-          }
-        `}
+    @keyframes scroll {
+      0% { transform: translateX(0); }
+      100% { transform: translateX(-50%); }
+    }
+  `}
       </style>
     </div>
   );
